7ef21617ac92a9fdc337b2fa2289dabee55601c2
mhmdakram <mhmdakram111@gmail.com>
2022-06-30 16:36:37 +0530

    First Update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -5,11 +5,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	for (var chart of charts) {
 		chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
 	}
-
-	var products = document.querySelectorAll('[data-bss-dynamic-product]');
-
-	for (var product of products) {
-		var param = product.dataset.bssDynamicProductParam;
-		product.dataset.reflowProduct = new URL(location.href).searchParams.get(param)
-	}
 }, false);
